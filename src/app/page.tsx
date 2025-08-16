b219571b456a919db1276113c1b289f941d67789
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2025-08-16 18:19:12 +0200

    Adicionando responsividade do container de listagem de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Next E-Commerce 13</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6 ">
+        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
+        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
+        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
+        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
+        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
+        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
+      </div>
     </div>
   );
 }
