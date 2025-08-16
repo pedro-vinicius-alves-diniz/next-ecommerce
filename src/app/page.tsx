7123ab3b80581cab73361220acc6ba17ce7792e0
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2025-08-16 18:38:38 +0200

    Adicionando tipagem de produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
+import { ProductType } from "./types/ProductType"
 
-export default function Home() {
+async function getProducts() {
+  const res = await fetch("https://fakestoreapi.com/products")
+
+  if(!res.ok){
+    throw new Error("Failed to fetch data")
+  }
+
+  return res.json()
+}
+
+
+export default async function Home() {
+  
+  const products = await getProducts()
+
+  console.log(products)
+
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6 ">
-        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
-        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
-        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
-        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
-        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
-        <div className="bg-zinc-50 p-8 rounded-2xl">Prod 1</div>
+        {products.map((product: ProductType) => {
+          return(
+            <div key={product.id}>
+              <h1>{product.title}</h1>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
